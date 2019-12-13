52e29133118e1f3a76f6793520f7e3938028d1e9
Denkodin <denkodin@gmail.com>
2019-12-14 00:55:06 +0300

    Added files for the video "How to animate a tool" at the channel "Easy Quest in Roblox Studio"

---
+++ 6_Tool_animation/LocalScript.lua
@@ -0,0 +1,19 @@
+local knife = script.Parent
+local id = 'rbxassetid://04501649960'
+
+knife.Activated:Connect(function()
+	if script.Parent.Parent:FindFirstChild("Humanoid") then
+		local anim = Instance.new("Animation")
+		anim.AnimationId = id
+		local loadedanim = script.Parent.Parent.Humanoid:LoadAnimation(anim)
+		loadedanim:Play()
+		local PlayingAnimationTracks = script.Parent.Parent.Humanoid:GetPlayingAnimationTracks()
+		for i, v in pairs(PlayingAnimationTracks) do
+			if v.Name == 'ToolNoneAnim' then
+				v:Stop()
+				repeat wait() until loadedanim.IsPlaying == false
+				v:Play()
+			end
+		end
+	end
+end)
