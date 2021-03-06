0e8082ddc7029f3b67ed004937d5f048eb8eb888
denkodin <denkodin@gmail.com>
2021-03-07 02:38:34 +0300

    Added license to use to the all source files

--- 001_Door1/DoorScript.lua
+++ 001_Door1/DoorScript.lua
@@ -1,56 +1,78 @@
+--[[                 Made by denkodin
+YouTube channels:
+https://www.youtube.com/c/H2MinRobloxStudio
+http://www.youtube.com/c/EasyQuestinRobloxStudio
+
+                      License to use
+
+What is allowed?
+You are allowed to use these scripts and this model in your
+games without restrictions, including monetize games.
+
+What is forbidden?
+It is forbidden to sell and distribute in any way these
+scripts and this model.
+Also you may not distribute the model incorporated in
+a derivative model (for instance, you may not made a modified
+version of it and sell it or distribute it).
+And you may not claim you are the creator or copyright holder
+of this model or these scripts.
+It is forbidden to remove this comment from the scripts.
+]]
+
 local PrimaryPart = script.Parent.PrimaryPart
 local Handle1 = script.Parent.Handle1
 local Handle2 = script.Parent.Handle2
 local Opened = false
 
 Handle1.ClickDetector.MouseClick:Connect(function()
-	
+
 	if Opened == false then
-		
+
 		Opened = true
 		for i = 1, 18 do
-			
+
 			script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(5), 0))
 			wait()
-			
+
 		end
-		
+
 	else
-		
+
 		Opened = false
 		for i = 1, 18 do
-			
+
 			script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(-5), 0))
 			wait()
-			
+
 		end
-		
+
 	end
-	
+
 end)
 
 Handle2.ClickDetector.MouseClick:Connect(function()
-	
+
 	if Opened == false then
-		
+
 		Opened = true
 		for i = 1, 18 do
-			
+
 			script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(5), 0))
 			wait()
-			
+
 		end
-		
+
 	else
-		
+
 		Opened = false
 		for i = 1, 18 do
-			
+
 			script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(-5), 0))
 			wait()
-			
+
 		end
-		
+
 	end
-	
+
 end)
--- 002_KeyDoor/OpenScript.lua
+++ 002_KeyDoor/OpenScript.lua
@@ -1,3 +1,25 @@
+--[[                 Made by denkodin
+YouTube channels:
+https://www.youtube.com/c/H2MinRobloxStudio
+http://www.youtube.com/c/EasyQuestinRobloxStudio
+
+                      License to use
+
+What is allowed?
+You are allowed to use these scripts and this model in your
+games without restrictions, including monetize games.
+
+What is forbidden?
+It is forbidden to sell and distribute in any way these
+scripts and this model.
+Also you may not distribute the model incorporated in
+a derivative model (for instance, you may not made a modified
+version of it and sell it or distribute it).
+And you may not claim you are the creator or copyright holder
+of this model or these scripts.
+It is forbidden to remove this comment from the scripts.
+]]
+
 local InnerLocker = script.Parent.InnerLockerV
 local Opened = script.Parent.Opened
 local DoorId = script.Parent.Id
@@ -5,73 +27,73 @@ local PrimaryPart = script.Parent.PrimaryPart
 local OpenDuration = 3
 
 script.Parent.Door.Touched:Connect(function(hit)
-	
+
 	if hit.Parent.Name == 'Key' and hit.Parent:FindFirstChild("Id") and hit.Parent.Id:IsA("NumberValue") and hit.Parent.Id.Value == DoorId.Value then
-		
+
 		if InnerLocker.Value == false then
-			
+
 			if Opened.Value == false then
-				
+
 				Opened.Value = true
-				
+
 				for i = 1, 21 do
-					
+
 					script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(5), 0))
 					wait()
-					
+
 				end
-				
+
 				wait(OpenDuration)
-				
+
 				for i = 1, 21 do
-					
+
 					script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(-5), 0))
 					wait()
-					
+
 				end
-				
+
 				Opened.Value = false
-				
+
 			end
-			
+
 		end
-		
+
 	end
-	
+
 end)
 
 script.Parent.I_E.Touched:Connect(function(hit)
-	
+
 	if hit.Parent:FindFirstChild("Humanoid") and hit.Name == "HumanoidRootPart" then
-		
+
 		if InnerLocker.Value == false then
-			
+
 			if Opened.Value == false then
-				
+
 				Opened.Value = true
-				
+
 				for i = 1, 21 do
-					
+
 					script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(5), 0))
 					wait()
-					
+
 				end
-				
+
 				wait(OpenDuration)
-				
+
 				for i = 1, 21 do
-					
+
 					script.Parent:SetPrimaryPartCFrame(PrimaryPart.CFrame * CFrame.Angles(0, math.rad(-5), 0))
 					wait()
-					
+
 				end
-				
+
 				Opened.Value = false
-				
+
 			end
-			
+
 		end
-		
+
 	end
-	
+
 end)
